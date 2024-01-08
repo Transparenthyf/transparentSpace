a2e52b0a8e3b98c0aaadc46b0132c15a0db3f85c
Transparenthyf <18098581870@163.com>
2024-01-08 19:56:27 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Footer from './components/Footer.vue'
+import { reactive } from 'vue'
+
+const view = reactive({
+  activeIndex: 1
+})
+</script>
+
+<template>
+  <el-menu :default-active="view.activeIndex" class="el-menu-demo" mode="horizontal">
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-sub-menu index="2"> 2 </el-sub-menu>
+    <el-menu-item index="3">3</el-menu-item>
+    <el-menu-item index="4">4</el-menu-item>
+  </el-menu>
+  <Footer></Footer>
+</template>
+
+<style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="footer">
+    <img src="../assets/image/备案图标.png" alt="备案图标" />
+    <a
+      target="_blank"
+      href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33011002017184"
+      style="display: inline-block; text-decoration: none; height: 20px; line-height: 20px"
+    >
+      <p style="float: left; height: 20px; line-height: 20px; margin: 0px 0px 0px 5px; color: #939393">
+        浙公网安备 33011002017184号
+      </p>
+    </a>
+
+    <a target="_blank" href="http://beian.miit.gov.cn">
+      <p style="float: left; height: 20px; line-height: 20px; margin: 0px 0px 0px 5px; color: #939393">
+        ICP主体备案号：浙ICP备2023013050号
+      </p>
+    </a>
+    <a target="_blank" href="http://beian.miit.gov.cn">
+      <p style="float: left; height: 20px; line-height: 20px; margin: 0px 0px 0px 5px; color: #939393">
+        网站备案号：浙ICP备2023013050号-1
+      </p>
+    </a>
+  </div>
+</template>
+
+<style scoped less>
+#footer {
+  position: absolute;
+  bottom: 0px;
+  padding: 5px;
+  height: 20px;
+  font-size: 10px;
+  a {
+    display: inline-block;
+  }
+}
+</style>
