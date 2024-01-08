556ad349d1ac31e4965133fd2efc1aa9c2b0a662
Transparent_H <18098581870@163.com>
2024-01-08 21:46:19 +0800

    增加 element plus 样式文件用于覆盖默认样式

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import './assets/less/base.less'
-
 import App from './App.vue'
+
+// element plus 基础样式
+import 'element-plus/dist/index.css'
+// 覆盖 element plus 样式
+import './assets/less/element.less'
+
+// 界面基础样式
+import './assets/less/base.less'
 
 const app = createApp(App)
 
