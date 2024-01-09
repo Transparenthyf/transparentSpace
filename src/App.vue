ec112ac1cc5934e63f6110dd50886fb2ee3fcef8
Transparent_H <18098581870@163.com>
2024-01-09 19:36:48 +0800

    增加页眉组件

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup lang="ts">
 import Footer from './components/Footer.vue'
-import { reactive } from 'vue'
-
-const view = reactive({
-  activeIndex: '1'
-})
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <el-menu id="menu" :default-active="view.activeIndex" mode="horizontal">
-    <el-menu-item index="1">Processing Center</el-menu-item>
-    <el-menu-item index="2">Processing Center</el-menu-item>
-    <el-menu-item index="3">Processing Center</el-menu-item>
-    <el-menu-item index="4">Processing Center</el-menu-item>
-  </el-menu>
+  <Header></Header>
   <Footer></Footer>
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const view = reactive({
+  activeIndex: '1'
+})
+</script>
+
+<template>
+  <el-menu id="menu" :default-active="view.activeIndex" :ellipsis="false" mode="horizontal">
+    <el-menu-item>logo</el-menu-item>
+    <div class="flex-grow" />
+    <el-menu-item index="1">个人简介</el-menu-item>
+    <el-menu-item index="2">项目实例</el-menu-item>
+    <el-menu-item index="5">关于本站</el-menu-item>
+  </el-menu>
+</template>
+
+<style scoped less>
+.flex-grow {
+  flex-grow: 1;
+}
+</style>
