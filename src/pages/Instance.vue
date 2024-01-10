de7ff774d7446971a9ad1af7a6d6d31bdb7d929a
Transparent_H <18098581870@163.com>
2024-01-10 21:26:50 +0800

    完成实例页面布局

--- src/pages/Instance.vue
+++ src/pages/Instance.vue
@@ -1,7 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const view = reactive({
+  colSpan: {
+    annotation: 7,
+    go: 7,
+    three: 7
+  }
+})
+</script>
 
 <template>
-  <div class="container">这里项目实例</div>
+  <div class="container">
+    <el-row justify="space-around">
+      <el-col :span="view.colSpan.annotation">
+        <el-card shadow="hover"> 标注平台 </el-card>
+      </el-col>
+      <el-col :span="view.colSpan.go">
+        <el-card shadow="hover"> Go.js 流程图 </el-card>
+      </el-col>
+      <el-col :span="view.colSpan.three">
+        <el-card shadow="hover"> Three.js 3D </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
-<style scoped less></style>
+<style scoped less>
+.el-row {
+  height: 100%;
+  .el-card {
+    height: 100%;
+  }
+}
+</style>
