84ffdeec87bd87234ea5c7667969fc73f6126e55
Transparent_H <18098581870@163.com>
2024-01-10 19:48:29 +0800

    引入 element 图标

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,24 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router/index.js'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 // element plus 基础样式
 import 'element-plus/dist/index.css'
 // 覆盖 element plus 样式
 import './assets/less/element.less'
-
 // 界面基础样式
 import './assets/less/base.less'
 
 const app = createApp(App)
 
+// 引入 element 图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+// 引入 element 组件库
 app.use(ElementPlus)
+// 引入路由
 app.use(router)
+
 app.mount('#app')
