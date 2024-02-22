62ea0239f06c5462b6c680f225827d688cd8091a
Transparent_H <18098581870@163.com>
2024-02-22 19:53:52 +0800

    添加 axios 插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
-    port: 2999
+    port: 2999,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
