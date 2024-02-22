d85a3f81d60ddbd537637d02dafae1a063ab801d
Transparent_H <18098581870@163.com>
2024-02-22 21:29:38 +0800

    基本完成加载简历功能

--- src/pages/Resume.vue
+++ src/pages/Resume.vue
@@ -1,7 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import axios from '../utils/axios'
+import { marked } from 'marked'
+import '../assets/less/markdown.less'
+
+let resumeHTML = ''
+let resumeContainer: Element | null = null
+
+onMounted(() => {
+  axios.get('/resume').then(async (res) => {
+    if (res.status === 200) {
+      resumeHTML = await marked(res.data)
+    }
+
+    resumeContainer = document.getElementById('resumeContainer')
+    if (resumeContainer !== null) {
+      resumeContainer.innerHTML = resumeHTML
+    }
+  })
+})
+</script>
 
 <template>
-  <div class="container">这里写个人简历</div>
+  <div class="container">
+    <div id="resumeContainer" class="markdown"></div>
+  </div>
 </template>
 
-<style scoped less></style>
+<style scoped less>
+#resumeContainer {
+  height: 100%;
+  overflow: auto;
+  padding: 0 50px;
+}
+</style>
