680f29f997ec54fb9533869596223f24eeb96638
Transparent_H <18098581870@163.com>
2024-02-23 16:11:39 +0800

    修改判断生产环境和开发环境的方式

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 let api = ''
 
-if (process.env !== undefined && process.env.NODE_ENV === 'development') {
+if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
   api = '/api'
 }
 
